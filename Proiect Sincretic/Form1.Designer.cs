357686ae57f9129da55e4eb3703f8bfe6af34a7d
Zsolt <talpai.zsolt@gmail.com>
2022-03-13 22:34:24 +0200

    Implemented array number generation and chart visualization

--- Proiect Sincretic/Form1.Designer.cs
+++ Proiect Sincretic/Form1.Designer.cs
@@ -28,15 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -44,16 +44,22 @@
             this.label4 = new System.Windows.Forms.Label();
             this.txt_maxvalue = new System.Windows.Forms.TextBox();
             this.btn_generate = new System.Windows.Forms.Button();
-            this.list_array = new System.Windows.Forms.ListBox();
             this.chart_bubble = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart_shell = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart_quick = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.lbl_execBubble = new System.Windows.Forms.Label();
+            this.lbl_execQuick = new System.Windows.Forms.Label();
+            this.list_Bubble = new System.Windows.Forms.ListBox();
+            this.list_Shell = new System.Windows.Forms.ListBox();
+            this.list_Quick = new System.Windows.Forms.ListBox();
             this.label8 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lbl_array = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart_bubble)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart_shell)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart_quick)).BeginInit();
@@ -123,59 +129,55 @@
             this.btn_generate.TabIndex = 6;
             this.btn_generate.Text = "Generate array";
             this.btn_generate.UseVisualStyleBackColor = true;
-            // 
-            // list_array
-            // 
-            this.list_array.FormattingEnabled = true;
-            this.list_array.Location = new System.Drawing.Point(302, 143);
-            this.list_array.Name = "list_array";
-            this.list_array.Size = new System.Drawing.Size(966, 17);
-            this.list_array.TabIndex = 7;
+            this.btn_generate.Click += new System.EventHandler(this.btn_generate_Click);
             // 
             // chart_bubble
             // 
-            chartArea4.Name = "ChartArea1";
-            this.chart_bubble.ChartAreas.Add(chartArea4);
-            legend4.Name = "Legend1";
-            this.chart_bubble.Legends.Add(legend4);
+            chartArea1.Name = "ChartArea1";
+            this.chart_bubble.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart_bubble.Legends.Add(legend1);
             this.chart_bubble.Location = new System.Drawing.Point(302, 215);
             this.chart_bubble.Name = "chart_bubble";
-            series4.ChartArea = "ChartArea1";
-            series4.Legend = "Legend1";
-            series4.Name = "Series1";
-            this.chart_bubble.Series.Add(series4);
+            series1.ChartArea = "ChartArea1";
+            series1.IsXValueIndexed = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Elements";
+            this.chart_bubble.Series.Add(series1);
             this.chart_bubble.Size = new System.Drawing.Size(289, 159);
             this.chart_bubble.TabIndex = 8;
             this.chart_bubble.Text = "chart1";
             // 
             // chart_shell
             // 
-            chartArea5.Name = "ChartArea1";
-            this.chart_shell.ChartAreas.Add(chartArea5);
-            legend5.Name = "Legend1";
-            this.chart_shell.Legends.Add(legend5);
-            this.chart_shell.Location = new System.Drawing.Point(607, 215);
+            chartArea2.Name = "ChartArea1";
+            this.chart_shell.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart_shell.Legends.Add(legend2);
+            this.chart_shell.Location = new System.Drawing.Point(616, 227);
             this.chart_shell.Name = "chart_shell";
-            series5.ChartArea = "ChartArea1";
-            series5.Legend = "Legend1";
-            series5.Name = "Series1";
-            this.chart_shell.Series.Add(series5);
+            series2.ChartArea = "ChartArea1";
+            series2.IsXValueIndexed = true;
+            series2.Legend = "Legend1";
+            series2.Name = "Elements";
+            this.chart_shell.Series.Add(series2);
             this.chart_shell.Size = new System.Drawing.Size(289, 159);
             this.chart_shell.TabIndex = 9;
             this.chart_shell.Text = "chart2";
             // 
             // chart_quick
             // 
-            chartArea6.Name = "ChartArea1";
-            this.chart_quick.ChartAreas.Add(chartArea6);
-            legend6.Name = "Legend1";
-            this.chart_quick.Legends.Add(legend6);
+            chartArea3.Name = "ChartArea1";
+            this.chart_quick.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart_quick.Legends.Add(legend3);
             this.chart_quick.Location = new System.Drawing.Point(911, 215);
             this.chart_quick.Name = "chart_quick";
-            series6.ChartArea = "ChartArea1";
-            series6.Legend = "Legend1";
-            series6.Name = "Series1";
-            this.chart_quick.Series.Add(series6);
+            series3.ChartArea = "ChartArea1";
+            series3.IsXValueIndexed = true;
+            series3.Legend = "Legend1";
+            series3.Name = "Elements";
+            this.chart_quick.Series.Add(series3);
             this.chart_quick.Size = new System.Drawing.Size(289, 159);
             this.chart_quick.TabIndex = 10;
             this.chart_quick.Text = "chart3";
@@ -207,35 +209,100 @@
             this.label7.TabIndex = 13;
             this.label7.Text = "Quicksort";
             // 
+            // lbl_execBubble
+            // 
+            this.lbl_execBubble.AutoSize = true;
+            this.lbl_execBubble.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_execBubble.Location = new System.Drawing.Point(344, 439);
+            this.lbl_execBubble.Name = "lbl_execBubble";
+            this.lbl_execBubble.Size = new System.Drawing.Size(98, 13);
+            this.lbl_execBubble.TabIndex = 14;
+            this.lbl_execBubble.Text = "Execution Time:";
+            // 
+            // lbl_execQuick
+            // 
+            this.lbl_execQuick.AutoSize = true;
+            this.lbl_execQuick.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_execQuick.Location = new System.Drawing.Point(957, 439);
+            this.lbl_execQuick.Name = "lbl_execQuick";
+            this.lbl_execQuick.Size = new System.Drawing.Size(98, 13);
+            this.lbl_execQuick.TabIndex = 18;
+            this.lbl_execQuick.Text = "Execution Time:";
+            // 
+            // list_Bubble
+            // 
+            this.list_Bubble.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.list_Bubble.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.list_Bubble.FormattingEnabled = true;
+            this.list_Bubble.Location = new System.Drawing.Point(448, 439);
+            this.list_Bubble.Name = "list_Bubble";
+            this.list_Bubble.Size = new System.Drawing.Size(92, 13);
+            this.list_Bubble.TabIndex = 19;
+            // 
+            // list_Shell
+            // 
+            this.list_Shell.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.list_Shell.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.list_Shell.FormattingEnabled = true;
+            this.list_Shell.Location = new System.Drawing.Point(757, 439);
+            this.list_Shell.Name = "list_Shell";
+            this.list_Shell.Size = new System.Drawing.Size(92, 13);
+            this.list_Shell.TabIndex = 20;
+            // 
+            // list_Quick
+            // 
+            this.list_Quick.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.list_Quick.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.list_Quick.FormattingEnabled = true;
+            this.list_Quick.Location = new System.Drawing.Point(1061, 439);
+            this.list_Quick.Name = "list_Quick";
+            this.list_Quick.Size = new System.Drawing.Size(92, 13);
+            this.list_Quick.TabIndex = 21;
+            // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label8.Location = new System.Drawing.Point(321, 399);
+            this.label8.Location = new System.Drawing.Point(653, 439);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(98, 13);
-            this.label8.TabIndex = 14;
+            this.label8.TabIndex = 22;
             this.label8.Text = "Execution Time:";
             // 
-            // label10
+            // button1
             // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label10.Location = new System.Drawing.Point(623, 399);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(94, 13);
-            this.label10.TabIndex = 16;
-            this.label10.Text = "Execution Time";
+            this.button1.Location = new System.Drawing.Point(398, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Sort";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // label12
+            // button2
             // 
-            this.label12.AutoSize = true;
-            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label12.Location = new System.Drawing.Point(945, 399);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(98, 13);
-            this.label12.TabIndex = 18;
-            this.label12.Text = "Execution Time:";
+            this.button2.Location = new System.Drawing.Point(710, 392);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "Sort";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1013, 392);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 25;
+            this.button3.Text = "Sort";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // lbl_array
+            // 
+            this.lbl_array.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_array.Location = new System.Drawing.Point(302, 143);
+            this.lbl_array.Name = "lbl_array";
+            this.lbl_array.Size = new System.Drawing.Size(900, 15);
+            this.lbl_array.TabIndex = 26;
             // 
             // Form1
             // 
@@ -243,16 +310,22 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuBar;
             this.ClientSize = new System.Drawing.Size(1309, 566);
-            this.Controls.Add(this.label12);
-            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lbl_array);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label8);
+            this.Controls.Add(this.list_Quick);
+            this.Controls.Add(this.list_Shell);
+            this.Controls.Add(this.list_Bubble);
+            this.Controls.Add(this.lbl_execQuick);
+            this.Controls.Add(this.lbl_execBubble);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.chart_quick);
             this.Controls.Add(this.chart_shell);
             this.Controls.Add(this.chart_bubble);
-            this.Controls.Add(this.list_array);
             this.Controls.Add(this.btn_generate);
             this.Controls.Add(this.txt_maxvalue);
             this.Controls.Add(this.label4);
@@ -278,16 +351,22 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txt_maxvalue;
         private System.Windows.Forms.Button btn_generate;
-        private System.Windows.Forms.ListBox list_array;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart_bubble;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart_shell;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart_quick;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbl_execBubble;
+        private System.Windows.Forms.Label lbl_execQuick;
+        private System.Windows.Forms.ListBox list_Bubble;
+        private System.Windows.Forms.ListBox list_Shell;
+        private System.Windows.Forms.ListBox list_Quick;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lbl_array;
     }
 }
 
--- Proiect Sincretic/Form1.cs
+++ Proiect Sincretic/Form1.cs
@@ -16,5 +16,46 @@ namespace Proiect_Sincretic
         {
             InitializeComponent();
         }
+        private void btn_generate_Click(object sender, EventArgs e)
+        {
+            string array_size = txt_array.Text;
+            string max_value = txt_maxvalue.Text;
+            string array_label="";
+            Random random = new Random();
+            int size = int.Parse(array_size);
+            int[] arr = new int[size];
+            if ((txt_maxvalue.Text == "") || (txt_array.Text == ""))
+                MessageBox.Show("Please insert values");
+            else
+            {
+               
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = random.Next(0, int.Parse(max_value));
+                    array_label +=arr[i] + " ";
+                }
+                lbl_array.Text = array_label;
+            }
+            for(int i=0; i < arr.Length; i++)
+            {
+                chart_bubble.Series["Elements"].Points.AddXY(arr[i], arr[i]);
+                chart_shell.Series["Elements"].Points.AddXY(arr[i], arr[i]);
+                chart_quick.Series["Elements"].Points.AddXY(arr[i], arr[i]);
+            }
+        }
+        private void fillchart1()
+        {
+            string size = txt_array.Text;
+            string array=lbl_array.Text;
+            int[] arr=new int[Convert.ToInt16(size)];
+            string[] elements = array.Split(' ');
+            foreach(string element in elements)
+            {
+                chart_bubble.Series["Elements"].Points.AddXY(element, size);
+            }
+            chart_bubble.Titles.Add("Bubblesort");
+
+
+        }
     }
 }
